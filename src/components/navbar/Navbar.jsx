f4e31d241a4943b305263ce92f0e43aecde6ee85
George Secen <W0533223@myscc.ca>
2024-12-04 22:13:12 -0500

    #1 Build desktop navigation
    - Added all of the needed nav bar items (Home, Experience etc)
    
    - Styled the nav bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,14 @@ import React from "react"
 import "./navbar.css"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="navbar">
+      <a href="">Home</a>
+      <a href="">Experience</a>
+      <a href="">Projects</a>
+      <a href="">Contact</a>
+    </div>
+  )
 }
 
 export default Navbar
