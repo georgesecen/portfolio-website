89d941aea65e2e2b642ec05e7928fe1e9531c668
George Secen <W0533223@myscc.ca>
2024-12-06 18:50:13 -0500

    #6 Change cursor on hover
    - Added functionality to animate the default cursor out and animate the custom cursor in when
    hovering over elements. This is done by adding and removing classes which show and unshow each
    cursor at the same time.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react"
-import { Navbar, Cursor } from "./components"
+import { Navbar, DefaultCursor, CustomCursor } from "./components"
 import "./myersReset.css"
 
 function App() {
   return (
     <div>
-      {/* <Navbar></Navbar> */}
-      <Cursor></Cursor>
+      <Navbar></Navbar>
+      <DefaultCursor></DefaultCursor>
+      <CustomCursor></CustomCursor>
+      <div
+        onMouseEnter={() => {
+          const g = document.getElementById("default-cursor")
+          const c = document.getElementById("custom-cursor")
+          g.classList.add("show")
+          c.classList.add("show")
+        }}
+        onMouseLeave={() => {
+          const g = document.getElementById("default-cursor")
+          const c = document.getElementById("custom-cursor")
+          g.classList.remove("show")
+          c.classList.remove("show")
+        }}
+      >
+        hover over me
+      </div>
     </div>
   )
 }
