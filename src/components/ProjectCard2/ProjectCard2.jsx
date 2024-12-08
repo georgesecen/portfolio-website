9ace2afe8ac11f846bffc145c7ea176e85fb187f
George Secen <W0533223@myscc.ca>
2024-12-07 22:27:28 -0500

    Add version 2 of the project card
    - Added a second version of the project card that functions the exact same except the project details
    are over the project image instead of underneath it

---
+++ src/components/ProjectCard2/ProjectCard2.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import "./projectCard2.css"
+import CodeLanguageIcon from "../CodeLanguageIcon/CodeLanguageIcon"
+
+const ProjectCard2 = ({ title, languages, image, link }) => {
+  // Function used to toggling custom cursor when hovering over project card
+  function toggleCustomCursor() {
+    const defaultCursor = document.getElementById("default-cursor")
+    const customCursor = document.getElementById("custom-cursor")
+    defaultCursor.classList.toggle("show")
+    customCursor.classList.toggle("show")
+  }
+
+  console.log(title)
+
+  return (
+    <a onMouseEnter={toggleCustomCursor} onMouseLeave={toggleCustomCursor} href={link} rel="noreferrer" target="_blank" className="project-card2">
+      <img src={image} alt="" />
+      <h2>{title}</h2>
+      <div className="project-tech2">
+        {languages.map((language, index) => {
+          return <CodeLanguageIcon key={index} language={language}></CodeLanguageIcon>
+        })}
+      </div>
+    </a>
+  )
+}
+
+export default ProjectCard2
