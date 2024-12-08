9392a8e1317f7c12f568f8bc1cdff0d57da00a79
George Secen <W0533223@myscc.ca>
2024-12-08 00:38:20 -0500

    #12 Animate project card image
    - Added functionality to input multiple images into the project card which will be changed in the
    project card background every 5 seconds
    
    - Removed react strict mode because it would cause images to not change as the code would run twice
    at a time and an image would be replaced with itself (if there were two images)

--- src/components/ProjectCard2/ProjectCard2.jsx
+++ src/components/ProjectCard2/ProjectCard2.jsx
@@ -1,8 +1,36 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import "./projectCard2.css"
 import CodeLanguageIcon from "../CodeLanguageIcon/CodeLanguageIcon"
 
-const ProjectCard2 = ({ title, languages, image, link }) => {
+// Note: Images should all be same size for animation to look the best
+const ProjectCard2 = ({ title, languages, images, link }) => {
+  // Create random id for image tag as there will be multiple project cards so we need to make sure
+  // we are animating the correct project card image
+  const id = Math.floor(Math.random() * 9999)
+
+  // Keep track of what index we are on in the images array
+  let index = useRef(1) // 1 as default image is images[0]
+
+  useEffect(() => {
+    // Function changes the image to the next image in the images array
+    function changeImage() {
+      // Get where index is in array as it may be out of bounds
+      const i = index.current % images.length
+
+      // Get element which has projects image as background
+      const currentImage = document.getElementById(id)
+
+      // Change background image to next image in images array
+      currentImage.style.backgroundImage = `url(${images[i]})`
+      index.current++
+    }
+
+    // Change image every 5 seconds only if there are multiple images
+    if (images.length > 0) {
+      setInterval(changeImage, 5000)
+    }
+  }, [])
+
   // Function used to toggling custom cursor when hovering over project card
   function toggleCustomCursor() {
     const defaultCursor = document.getElementById("default-cursor")
@@ -11,11 +39,14 @@ const ProjectCard2 = ({ title, languages, image, link }) => {
     customCursor.classList.toggle("show")
   }
 
-  console.log(title)
-
   return (
     <a onMouseEnter={toggleCustomCursor} onMouseLeave={toggleCustomCursor} href={link} rel="noreferrer" target="_blank" className="project-card2">
-      <img src={image} alt="" />
+      <div className="image-container">
+        {/* Image is kind of a dummy image its just used to fill up the size in card for image changer to cover */}
+        <img src={images[0]} alt="" />
+        {/* Div will keep changing background image */}
+        <div className="image-changer" style={{ backgroundImage: `url(${images[0]}` }} id={id}></div>
+      </div>
       <h2>{title}</h2>
       <div className="project-tech2">
         {languages.map((language, index) => {
