bf17bb2a4b0973faca4dd4afe227bae306c2b961
George Secen <W0533223@myscc.ca>
2024-12-08 20:01:19 -0500

    #15 Style 3d bar
    - Gave the bar more box shadow and a bottom face to give it more depth and make it look more 3d
    
    - Added colored sides to the faces of the bar which are visual to the user which cover a ceratin
    percentage of the bar so it can show some information
    
    - Added a percentage number that sticks to the growing bar on the faces so that the user can see
     what percentage the bar represents
    
    - Added a label which sticks to the bottom of the bar

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,13 +1,24 @@
 import React from "react"
 import "./bar.css"
+import CodeLanguageIcon from "../CodeLanguageIcon/CodeLanguageIcon"
 
 const Bar = () => {
   return (
-    <div className="bar">
-      {/* Only left, right, and top face are visible to the user */}
-      <div className="left-face"></div>
-      <div className="right-face"></div>
-      <div className="top-face"></div>
+    <div className="bar-container">
+      <div className="bar">
+        {/* Only left, right, and top face are visible to the user, however bottom face adds box shadow to make shape more 3d */}
+        <div className="left-face">
+          {/* The growing bar is the colored slider which translates up and down the rectangle face */}
+          <div className="growing-bar"></div>
+        </div>
+        <div className="right-face">
+          {/* The growing bar is the colored slider which translates up and down the rectangle face */}
+          <div className="growing-bar"></div>
+        </div>
+        <div className="top-face"></div>
+        <div className="bottom-face"></div>
+      </div>
+      <div className="label">Test text</div>
     </div>
   )
 }
