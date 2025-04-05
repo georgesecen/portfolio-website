9b5b685cd4bd65fca618df4f2620906795115293
George Secen <W0533223@myscc.ca>
2025-04-05 14:19:54 -0400

    Create IntroductionSection component
    - Created the IntroductionSection component and added some basic hello my name is... text

---
+++ src/components/IntroductionSection/IntroductionSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./introductionSection.css"
+
+const IntroductionSection = () => {
+  return (
+    <div className='introduction-section-container'>
+        <h4>&#128075; Hello, I'm</h4>
+        <h1>George Secen</h1>
+        <h4>A Software Developer based in Canada&#127809;</h4>
+    </div>
+  )
+}
+
+export default IntroductionSection
