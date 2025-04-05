d8413dfa8ca526918037ffe6d36d9f566c805954
George Secen <W0533223@myscc.ca>
2025-04-05 14:37:00 -0400

    #23 Style introduction text

--- src/components/IntroductionSection/IntroductionSection.jsx
+++ src/components/IntroductionSection/IntroductionSection.jsx
@@ -4,9 +4,11 @@ import "./introductionSection.css"
 const IntroductionSection = () => {
   return (
     <div className='introduction-section-container'>
-        <h4>&#128075; Hello, I'm</h4>
-        <h1>George Secen</h1>
-        <h4>A Software Developer based in Canada&#127809;</h4>
+        <p>
+            <h4>&#128075; Hello, I'm</h4>
+            <h1>George Secen</h1>
+            <h4>A <span>Software Developer</span> Based In Canada&#127809;</h4>
+        </p>
     </div>
   )
 }
