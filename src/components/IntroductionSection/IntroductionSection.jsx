db505810f0a75c8b6e944d0b8a1cfe5343686bee
George Secen <W0533223@myscc.ca>
2025-04-05 17:07:40 -0400

    Doc comment IntroductionSection component

--- src/components/IntroductionSection/IntroductionSection.jsx
+++ src/components/IntroductionSection/IntroductionSection.jsx
@@ -11,15 +11,23 @@ const githubLink = "https://github.com/georgesecen"
 const linkedinLink = "https://www.linkedin.com/in/george-secen-0084a9351/"
 const emailLink = "mailto:george.secen.work@gmail.com"
 
+/**
+ * Introduction section displays hello my name is ... text. It also contains links to my socials so
+ * users can contact me and view my work. 
+ * @returns {React.JSX.Element} IntroductionSection react component.
+ */
 const IntroductionSection = () => {
   return (
     <div className='introduction-section-container'>
+
+        {/* Intro text */}
         <section>
             <h4>&#128075; Hello, I'm</h4>
             <h1>George Secen</h1>
             <h4>A <span>Software Developer</span> Based In Canada&#127809;</h4>
         </section>
 
+        {/* All socials links */}
         <div>
             {/* TODO: Make all buttons look better */}
             <a className='cv-button' href={`${curriculumVitae}`} rel="noreferrer" target="_blank">
