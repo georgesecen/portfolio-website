c94662e6487bbff55e73f8b7c59d9c2bcfc8ae7a
George Secen <W0533223@myscc.ca>
2025-04-13 18:11:54 -0400

    #30 Add my experiences to the ExperiencesSection component

--- src/sections/ExperiencesSection/ExperiencesSection.jsx
+++ src/sections/ExperiencesSection/ExperiencesSection.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 import { ExperienceCard } from '../../components'
 import "./experiencesSection.css"
 
+/**
+ * Experiences section which displays my experiences using ExperienceCard components.
+ * @returns {React.JSX.Element} ExperiencesSection React component.
+ */
 const ExperiencesSection = () => {
   return (
     <div className='experiences-section'>
         <div>
             <ExperienceCard 
                 title="Computer Science Tutor" 
-                description="St.Clair College" 
-                date="January 1970 - December 2050" 
+                description="St.Clair College, Student Services" 
+                date="September 2023 - Present" 
+                type="Hybrid"
+                >
+            </ExperienceCard>
+            <ExperienceCard 
+                title="WinHacks Hackathon" 
+                description="University of Windsor" 
+                date="Febuary 15 2025 - Febuary 16 2025" 
                 type="On Site"
                 >
             </ExperienceCard>
             <ExperienceCard 
-                title="Computer Science Tutor" 
+                title="CiCan Hackathon" 
                 description="St.Clair College" 
-                date="January 1970 - December 2050" 
-                type="On Site"
-                >
-            </ExperienceCard>
-            <ExperienceCard 
-                title="Computer Science Tutor" 
-                description="St.Clair College" 
-                date="January 1970 - December 2050" 
+                date="March 22 2025 - March 23 2025" 
                 type="On Site"
                 >
             </ExperienceCard>
