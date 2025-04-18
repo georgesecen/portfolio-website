7c01c96c06cddc9250dbf278c340b5dfe664aaad
George Secen <W0533223@myscc.ca>
2025-04-18 16:02:00 -0400

    Re-style experience card component
    - Styled the experience card component so it blends in with the new fonts and colors added to
    the website

--- src/components/ExperienceCard/ExperienceCard.jsx
+++ src/components/ExperienceCard/ExperienceCard.jsx
@@ -7,13 +7,14 @@ import "./experienceCard.css"
  * @param {string} description Very short description of experience or location if the experience is for a job. Should be max 5 words.
  * @param {string} date Date of experience. (Ex. January 2020 - March 2021)
  * @param {string} type Type of experience. (On Site, Hybrid, Remote)
+ * @param {string} icon Icon to be placed after experience title. (Optional)
  * @returns {React.JSX.Element} ExperienceCard React component.
  */
-const ExperienceCard = ({ title, description, date, type }) => {
+const ExperienceCard = ({ title, description, date, type, icon }) => {
   return (
     <div className="experience-card">
       <div className="left-side-details">
-        <h2>{title}</h2>
+        <h2>{title} <span><img src={icon} alt="" /></span></h2>
         <p>{description}</p>
       </div>
 
--- src/sections/ExperiencesSection/ExperiencesSection.jsx
+++ src/sections/ExperiencesSection/ExperiencesSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ExperienceCard } from '../../components'
+import { laptopIcon, codeIcon, schoolIcon } from '../../assets/images'
 import "./experiencesSection.css"
 
 /**
@@ -9,14 +10,14 @@ import "./experiencesSection.css"
 const ExperiencesSection = () => {
   return (
     <div id='experience-section' className='experiences-section' name="section">
-        <h1>Experience</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus eos est explicabo consequuntur beatae. Blanditiis, ad.</p>
+        <h1>&#127941; Experience</h1>
         <div>
             <ExperienceCard 
                 title="Computer Science Tutor" 
                 description="St.Clair College, Student Services" 
                 date="September 2023 - Present" 
                 type="Hybrid"
+                icon={schoolIcon}
                 >
             </ExperienceCard>
             <ExperienceCard 
@@ -24,6 +25,7 @@ const ExperiencesSection = () => {
                 description="University of Windsor" 
                 date="Febuary 15 2025 - Febuary 16 2025" 
                 type="On Site"
+                icon={laptopIcon}
                 >
             </ExperienceCard>
             <ExperienceCard 
@@ -31,6 +33,7 @@ const ExperiencesSection = () => {
                 description="St.Clair College" 
                 date="March 22 2025 - March 23 2025" 
                 type="On Site"
+                icon={codeIcon}
                 >
             </ExperienceCard>
         </div>
