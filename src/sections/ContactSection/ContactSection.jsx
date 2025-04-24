24f5ae2ae767f40829af84550a52c3b588037546
George Secen <W0533223@myscc.ca>
2025-04-24 17:08:59 -0400

    Add link to email in contact section

--- src/sections/ContactSection/ContactSection.jsx
+++ src/sections/ContactSection/ContactSection.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./contactSection.css"
 
+// Get links to CV and socials
+const curriculumVitae = require("../../assets/CV/cv.pdf")
+const githubLink = "https://github.com/georgesecen"
+const linkedinLink = "https://www.linkedin.com/in/george-secen-0084a9351/"
+const emailLink = "mailto:george.secen.work@gmail.com"
+
 const ContactSection = () => {
   return (
     <div id='contact-section' className='contact-section' name="section">
-      Contact Section
+      <h1>&#128222; Contact</h1>
+      <p>Let's shake hands <span>&#129309;</span></p>
+
+      <a href={`${emailLink}`} rel="noreferrer" target="_blank">george.secen.work@gmail.com</a>
     </div>
   )
 }
