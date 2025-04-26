92b8428289b800d9ce92c2cb478e1a1bfa84d9b5
George Secen <W0533223@myscc.ca>
2025-04-26 15:39:29 -0400

    Animate mobile navbar on button click
    - Added functionality so that the mobile menu slides out when the mobile menu button is clicked

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -7,14 +7,32 @@ const MobileNavbar = () => {
   const [active, setActive] = useState(false)
 
   return (
-    <div className='mobile-navigation'>
+    <div className={`mobile-navigation ${active ? "active" : ""}`}>
         
         {/* Nav button */}
         {/* Idea from https://codepen.io/shieldsma91/pen/zLpbLX */}
-        <div onClick={() => setActive(!active)} className={`nav-button ${active ? "active" : ""}`}>
+        <div onClick={() => setActive(!active)} className='nav-button'>
             <span></span>
             <span></span>
             <span></span>
+        </div>
+
+        <div className='menu'>
+            <a onClick={() => setActive(false)} href="#introduction-section">
+                Home
+            </a>
+            <a onClick={() => setActive(false)} href="#skills-section">
+                Skills
+            </a>
+            <a onClick={() => setActive(false)} href="#projects-section">
+                Projects
+            </a>
+            <a onClick={() => setActive(false)} href="#experience-section">
+                Experience
+            </a>
+            <a onClick={() => setActive(false)} href="#contact-section">
+                Contact
+            </a>
         </div>
 
     </div>
