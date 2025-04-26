04a2a54d1ded6227f79db55f0577598c0f14ede7
George Secen <W0533223@myscc.ca>
2025-04-26 15:08:47 -0400

    Add mobile navbar button

--- src/components/MobileNavbar/MobileNavbar.jsx
+++ src/components/MobileNavbar/MobileNavbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./mobileNavbar.css"
 
 const MobileNavbar = () => {
+
+  // Keep track if the mobile navbar is currently displayed
+  const [active, setActive] = useState(false)
+
   return (
-    <div>MobileNavbar</div>
+    <div className='mobile-navigation'>
+        
+        {/* Nav button */}
+        {/* Idea from https://codepen.io/shieldsma91/pen/zLpbLX */}
+        <div onClick={() => setActive(!active)} className={`nav-button ${active ? "active" : ""}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+    </div>
   )
 }
 
