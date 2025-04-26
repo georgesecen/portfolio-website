a371dccb8c582b2d395ad75b803df42f9827cf27
George Secen <W0533223@myscc.ca>
2025-04-26 16:00:28 -0400

    Make navbars responsive
    - Added media queries so that the mobile navbar is only showing on small devices
    
    - Added media queries so that regular navbar only shows on larger devices
    
    - Added media queries so that the custom cursors do not get displayed on smaller devices
    
    - Styled the mobile navbar

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     <div>
       <DefaultCursor />
       <CustomCursor />
-      {/* <Navbar /> */}
+      <Navbar />
       <MobileNavbar />
       <IntroductionSection />
       <SkillsSection />
